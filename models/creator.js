c52e8c4163e954f8bf97670b96821d9c0b02f286
pramit-marattha <pramitmarattha5497@gmail.com>
2022-07-13 00:46:04 +0545

    Add methods in creator schema

--- models/creator.js
+++ models/creator.js
@@ -52,3 +52,16 @@ creatorSchema.virtual("password")
         return this._password;
     }
 );
+
+creatorSchema.methods = {
+    encryptPassword: function (password) {
+        if (!password) return "";
+        try {
+            return crypto.createHmac("sha1", this.salt)
+                .update(password)
+                .digest("hex");
+        } catch (err) {
+            return "";
+        }
+    }
+}
