ca5f91bb3b92a81af61536bd25d9cf608308b1c8
pramit-marattha <pramitmarattha5497@gmail.com>
2022-07-13 00:05:40 +0545

    Fix: useCreateIndex issue

--- app.js
+++ app.js
@@ -22,8 +22,7 @@ app.get("/api/articles", (req, res) => {
 // db connection
 mongoose.connect(process.env.MONGODB_URI, { 
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
+    useUnifiedTopology: true
  }).then(() => {
     console.log("Connected to MongoDB");
 }
