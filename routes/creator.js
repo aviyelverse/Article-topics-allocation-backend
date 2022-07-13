5ee31a749d3d89bb737657a98e96b827cd5929ce
pramitmarattha <pramitmarattha5497@gmail.com>
2022-07-13 17:34:05 +0545

    Article allocation backend (#7)
    * Add signout controller
    
    * Add: protecting route middleware

--- routes/creator.js
+++ routes/creator.js
@@ -2,10 +2,15 @@ import express from "express";
 const router = express.Router();
 import {creatorSignUpValidator} from "../validator/validate.js";
 
-import {signup,login,logout} from "../controllers/creator.js";
+import {signup,login,logout,requireLogin} from "../controllers/creator.js";
 
 router.post("/signup", creatorSignUpValidator, signup);
 router.post("/login", login);
 router.get("/logout", logout);
+
+router.get("/test", requireLogin, (req, res) => {
+    res.send("You are logged in");
+    console.log("req.auth", req.auth);
+});
 
 export default router;
