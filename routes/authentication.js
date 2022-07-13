cca2a9c3de625e1432e81deb46784e80dabbacd8
pramit-marattha <pramitmarattha5497@gmail.com>
2022-07-13 20:23:41 +0545

    Update: renamed to authentication

--- routes/authentication.js
+++ routes/authentication.js
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 import {creatorSignUpValidator} from "../validator/validate.js";
 
-import {signup,login,logout,requireLogin} from "../controllers/creator.js";
+import {signup,login,logout,requireLogin} from "../controllers/authentication.js";
 
 router.post("/signup", creatorSignUpValidator, signup);
 router.post("/login", login);
