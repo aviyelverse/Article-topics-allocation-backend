cf97a7962012bb24ce2f9a015df12fb007203b78
pramit-marattha <pramitmarattha5497@gmail.com>
2022-07-14 09:46:12 +0545

    Add article model

---
+++ models/article.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+const {ObjectId} = mongoose.Schema;
+
+const articleSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            required: true,
+            maxlength: 32
+        },
+        description: {
+            type: String,
+            required: true,
+            maxlength: 2000
+        },
+        project: {
+            type: ObjectId,
+            ref: "Project",
+            required: true
+        },
+        quantity: {
+            type: Number
+        },
+        photo: {
+            data: Buffer,
+            contentType: String
+        },
+        assign: {
+            required: false,
+            type: Boolean
+        }
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("Article", articleSchema);
