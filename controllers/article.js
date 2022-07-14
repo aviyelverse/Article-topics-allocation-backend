b6d8a5450425efdb4612aee837718977b06c55b9
pramit-marattha <pramitmarattha5497@gmail.com>
2022-07-14 11:26:00 +0545

    Add article update functionality

--- controllers/article.js
+++ controllers/article.js
@@ -89,5 +89,60 @@ const deleteArticle = (req, res) => {
     });
 }
 
+const updateArticle = (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            return res.status(400).json({
+                error: "Article project Images could not be uploaded"
+            });
+        }
 
-export {createArticle, articleById, readArticle, deleteArticle};
+        // check all fields 
+        const {
+            name,
+            description,
+            project,
+            quantity,
+            assigned
+        } = fields;
+
+        if (
+            !name ||
+            !description ||
+            !project ||
+            !quantity ||
+            !assigned
+        ) {
+            return res.status(400).json({
+                error: "Fields required"
+            });
+        }
+
+        let article = req.article;
+        article = _.extend(article, fields);
+
+        if (files.photo) {
+            if (files.photo.size > 7000000) {
+                return res.status(400).json({
+                    error: "Image too large"
+                });
+            }
+            article.photo.data = fs.readFileSync(files.photo.path);
+            article.photo.contentType = files.photo.type;
+        }
+
+        article.save((err, result) => {
+            if (err) {
+                return res.staus(400).json({
+                    error: dbErrorHandle(err)
+                });
+            }
+            res.json(result);
+        });
+    });
+}
+
+
+export {createArticle, articleById, readArticle, deleteArticle, updateArticle};
--- routes/article.js
+++ routes/article.js
@@ -1,13 +1,14 @@
 import express from "express";
 const router = express.Router();
 
-import {createArticle, articleById, readArticle, deleteArticle} from "../controllers/article.js";
+import {createArticle, articleById, readArticle, deleteArticle, updateArticle} from "../controllers/article.js";
 import {requireLogin, isAuthenticated, isMaintainer} from "../controllers/authentication.js";
 import {creatorById} from "../controllers/creator.js";
 
 router.get("/article/:articletId", readArticle);
 router.post("/article/create-article/:creatorId", requireLogin, isAuthenticated, isMaintainer, createArticle);
 router.delete("/article/:articleId/:creatorId", requireLogin, isAuthenticated, isMaintainer, deleteArticle);
+router.put("/article/:articleId/:creatorId", requireLogin, isAuthenticated, isMaintainer, updateArticle);
 
 router.param("creatorId", creatorById);
 router.param("articleId", articleById);
