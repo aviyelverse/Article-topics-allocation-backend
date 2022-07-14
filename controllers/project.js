28198f209192c419a5fc5507456bfd0fa3a1491e
pramitmarattha <pramitmarattha5497@gmail.com>
2022-07-14 13:23:56 +0545

    projects middleware ( crud functionality )  (#12)
    * Add signout controller
    
    * Add: protecting route middleware
    
    * Update: renamed to authentication
    
    * Add creator by id middleware
    
    * Add autentication and maintainern middleware
    
    * Add project model route and controller
    
    * Auth admin only to add project
    
    * Add article model
    
    * Add article model
    
    * Add article with formidable
    
    * Add article field validation
    
    * Add article id middleware and single article
    
    * Add article delete middleware
    
    * Add article update functionality
    
    * Add project by id and single project
    
    * Add projects update delete and listAll middleware

--- controllers/project.js
+++ controllers/project.js
@@ -1,6 +1,19 @@
 import Project from "../models/project.js";
 import {dbErrorHandle} from "../helpers/databaseErrorHandle.js";
 
+
+const projectById = (req, res, next, id) => {
+    Project.findById(id).exec((err, project) => {
+        if (err || !project) {
+            return res.status(400).json({
+                error: "project not found"
+            });
+        }
+        req.project = project;
+        next();
+    });
+}
+
 const createProject = (req, res) => {
     const project = new Project(req.body);
     project.save((err, data) => {
@@ -13,4 +26,45 @@ const createProject = (req, res) => {
     });
 };
 
-export {createProject};
+const readProject = (req, res) => {
+    return res.json(req.project);
+}
+
+const updateProject = (req, res) => {
+    const project = req.project;
+    project.name = req.body.name;
+    project.save((err, data) => {
+        if (err) {
+            return res.status(400).json({
+                error: dbErrorHandle(err)
+            });
+        }
+
+        res.json({ data });
+    });
+}
+
+const deleteProject = (req, res) => {
+    const project = req.project;
+    project.remove((err, data) => {
+        if (err) {
+            return res.status(400).json({
+                error: dbErrorHandle(err)
+            });
+        }
+        res.json({ message: "deleted successfully" });
+    });
+}
+
+const allProjects = (req, res) => {
+    Project.find().exec((err, projects) => {
+        if (err) {
+            return res.status(400).json({
+                error: dbErrorHandle(err)
+            });
+        }
+        res.json(projects);
+    });
+}
+
+export {createProject, projectById, readProject, updateProject, deleteProject, allProjects};
--- routes/project.js
+++ routes/project.js
@@ -1,13 +1,17 @@
 import express from "express";
 const router = express.Router();
 
-import {createProject} from "../controllers/project.js";
+import {createProject, projectById, readProject, updateProject, deleteProject, allProjects} from "../controllers/project.js";
 import {requireLogin, isAuthenticated, isMaintainer} from "../controllers/authentication.js";
 import {creatorById} from "../controllers/creator.js";
 
-
+router.get("/project/:projectId", readProject);
 router.post("/project/create-project/:creatorId", requireLogin, isAuthenticated, isMaintainer, createProject);
+router.put("/project/:projectId/:creatorId", requireLogin, isAuthenticated, isMaintainer, updateProject);
+router.delete("/project/:projectId/:creatorId", requireLogin, isAuthenticated, isMaintainer, deleteProject);
+router.get("/projects",allProjects);
 
+router.param("projectId", projectById);
 router.param("creatorId", creatorById);
 
 export default router;
