668d5e53a0c32b910a3eff67fb1dd8abbc99da06
pramitmarattha <pramitmarattha5497@gmail.com>
2022-07-14 11:28:02 +0545

    article field validation routes middleware (crud functionality) (#11)
    * Add signout controller
    
    * Add: protecting route middleware
    
    * Update: renamed to authentication
    
    * Add creator by id middleware
    
    * Add autentication and maintainern middleware
    
    * Add project model route and controller
    
    * Auth admin only to add project
    
    * Add article model
    
    * Add article model
    
    * Add article with formidable
    
    * Add article field validation
    
    * Add article id middleware and single article
    
    * Add article delete middleware
    
    * Add article update functionality

--- controllers/article.js
+++ controllers/article.js
@@ -4,6 +4,24 @@ import fs from "fs";
 import Article from "../models/article.js";
 import {dbErrorHandle} from "../helpers/databaseErrorHandle.js";
 
+
+const articleById = (req, res, next, id) => {
+    Article.findById(id).exec((err, article) => {
+        if (err || !article) {
+            return res.status(400).json({
+                error: "article not found"
+            });
+        }
+        req.article = article;
+        next();
+    });
+}
+
+const readArticle = (req, res) => {
+    req.article.photo = undefined;
+    return res.json(req.article);
+}
+
 const createArticle = (req, res) => {
     let form = new formidable.IncomingForm();
     form.keepExtensions = true;
@@ -13,9 +31,36 @@ const createArticle = (req, res) => {
                 error: "Article project Images could not be uploaded"
             });
         }
+
+        // check all fields 
+        const {
+            name,
+            description,
+            project,
+            quantity,
+            assigned
+        } = fields;
+
+        if (
+            !name ||
+            !description ||
+            !project ||
+            !quantity ||
+            !assigned
+        ) {
+            return res.status(400).json({
+                error: "Fields required"
+            });
+        }
+
         let article = new Article(fields);
 
         if (files.photo) {
+            if (files.photo.size > 7000000) {
+                return res.status(400).json({
+                    error: "Image too large"
+                });
+            }
             article.photo.data = fs.readFileSync(files.photo.path);
             article.photo.contentType = files.photo.type;
         }
@@ -32,4 +77,72 @@ const createArticle = (req, res) => {
 };
 
 
-export {createArticle};
+const deleteArticle = (req, res) => {
+    let article = req.article;
+    article.remove((err, article) => {
+        if (err) {
+            return res.status(400).json({
+                error: dbErrorHandle(err)
+            });
+        }
+        res.json({message: "Article deleted successfully" });
+    });
+}
+
+const updateArticle = (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            return res.status(400).json({
+                error: "Article project Images could not be uploaded"
+            });
+        }
+
+        // check all fields 
+        const {
+            name,
+            description,
+            project,
+            quantity,
+            assigned
+        } = fields;
+
+        if (
+            !name ||
+            !description ||
+            !project ||
+            !quantity ||
+            !assigned
+        ) {
+            return res.status(400).json({
+                error: "Fields required"
+            });
+        }
+
+        let article = req.article;
+        article = _.extend(article, fields);
+
+        if (files.photo) {
+            if (files.photo.size > 7000000) {
+                return res.status(400).json({
+                    error: "Image too large"
+                });
+            }
+            article.photo.data = fs.readFileSync(files.photo.path);
+            article.photo.contentType = files.photo.type;
+        }
+
+        article.save((err, result) => {
+            if (err) {
+                return res.staus(400).json({
+                    error: dbErrorHandle(err)
+                });
+            }
+            res.json(result);
+        });
+    });
+}
+
+
+export {createArticle, articleById, readArticle, deleteArticle, updateArticle};
