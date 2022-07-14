075989bf3848655e628246f27870147a8b410b5b
pramit-marattha <pramitmarattha5497@gmail.com>
2022-07-14 09:37:46 +0545

    Auth admin only to add project

--- routes/project.js
+++ routes/project.js
@@ -2,7 +2,12 @@ import express from "express";
 const router = express.Router();
 
 import {createProject} from "../controllers/project.js";
+import {requireLogin, isAuthenticated, isMaintainer} from "../controllers/authentication.js";
+import {creatorById} from "../controllers/creator.js";
 
-router.post("/project/create-project", createProject);
+
+router.post("/project/create-project/:creatorId", requireLogin, isAuthenticated, isMaintainer, createProject);
+
+router.param("creatorId", creatorById);
 
 export default router;
